56cb678111a4cc77fd02e2f4633b2ad57423a6eb
venkatesh-cloudside <venkatesh.m@thecloudside.com>
2023-08-25 16:19:35 +0530

    newfile

---
+++ practice_python.py
@@ -0,0 +1,21 @@
+# tm = int(input("Enter Your Tamil Marks: "))
+# en = int(input("Enter Your English Marks: "))
+# ma = int(input("Enter Your Maths Marks: "))
+# sc = int(input("Enter Your Science Marks: "))
+# sl = int(input("Enter Your Social Marks: "))
+
+# av = (tm+en+ma+sc+sl)/5
+
+
+# if (tm > 35 and en > 35 and ma > 35 and sc > 35 and sl > 35):
+#     if (av >= 175):
+#         print("You Need Additional Classes")
+#     else:
+#         print('''You Don't Need any Additional Classes
+# Good Luck...!''')
+# else:
+#     print("You Need Additional Classes")
+
+
+for i in range(2, 6):
+    print(i)
